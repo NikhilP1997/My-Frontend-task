4929642a5812bf7f67854862ed6203d481e1e5d1
NikhilP1997 <157241317+NikhilP1997@users.noreply.github.com>
2024-07-10 18:18:43 +0530

    Update environment.prod.ts
    1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiGatewayURL: 'http://Application-LB-2088643759.us-east-2.elb.amazonaws.com'
+  apiGatewayURL: 'http://test-alb-1189752668.us-east-2.elb.amazonaws.com'
 };
